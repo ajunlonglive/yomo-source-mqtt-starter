c69fa940df495e232ae5ab10add8239611b4c9fa
chenjunbiao <chenjunbiao@outlook.com>
2021-04-15 19:08:45 +0800

    refactor dockerfile
    - create image for quic-mqtt
    - update dockerfile of noise and emit to base quic-mqtt

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM golang:buster
+
+RUN apt-get update && \
+    apt-get install nano iputils-ping telnet net-tools ifstat -y
+
+RUN cp  /usr/share/zoneinfo/Asia/Shanghai /etc/localtime  && \
+    echo 'Asia/Shanghai'  > /etc/timezone
+
+EXPOSE 1883
--- cmd/simplest/main.go
+++ cmd/simplest/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"os"
+
 	"github.com/yomorun/yomo-source-mqtt-starter/pkg/receiver"
 )
 
 func main() {
-	receiver.CreateRunner("localhost:9999").Run()
+	receiver.CreateRunner(os.Getenv("YOMO_SOURCE_MQTT_ZIPPER_ADDR")).Run()
 }
